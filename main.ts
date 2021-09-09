7108bb54ac1336b6995adf237d0df13db8b24459
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-09-09 20:00:59 +0100

    set delay to 50ms to allow time for pulse to be recieved back

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
+let Dist = 0
 basic.forever(function () {
-    basic.showString("" + (sonar.ping(
+    Dist = sonar.ping(
     DigitalPin.P2,
     DigitalPin.P16,
-    PingUnit.Centimeters
-    )))
-    basic.pause(500)
+    PingUnit.MicroSeconds
+    )
+    basic.pause(50)
+    kitronik_VIEW128x64.show(Dist, 1, kitronik_VIEW128x64.ShowAlign.Left)
+    kitronik_VIEW128x64.plot(Dist)
 })
