3e5defecc7a3f7fde16cb6f3af87a83447e3a1f2
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-09-09 20:05:34 +0100

    set to maual reading via A_Button

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
-let Dist = 0
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     Dist = sonar.ping(
     DigitalPin.P2,
     DigitalPin.P16,
-    PingUnit.MicroSeconds
+    PingUnit.Centimeters
     )
     basic.pause(50)
     kitronik_VIEW128x64.show(Dist, 1, kitronik_VIEW128x64.ShowAlign.Left)
     kitronik_VIEW128x64.plot(Dist)
+})
+let Dist = 0
+kitronik_VIEW128x64.clear()
+basic.forever(function () {
+	
 })
